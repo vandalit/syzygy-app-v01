edac9f58513339deaef42791121687963e7cd381
Van <84937597+vandalit@users.noreply.github.com>
2025-01-07 23:37:05 -0300

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB74lyzgYjsHMDTrUG9JUgRf5k8576fDI8",
+  authDomain: "syzygy-design.firebaseapp.com",
+  projectId: "syzygy-design",
+  storageBucket: "syzygy-design.firebasestorage.app",
+  messagingSenderId: "720239787814",
+  appId: "1:720239787814:web:7b370c28bf7887cf6e0be7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { db } from "./firebase";
+
+console.log("Firestore conectado:", db);
+
+
 createApp(App).use(store).use(router).mount('#app')
+
